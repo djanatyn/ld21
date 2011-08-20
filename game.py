6de5e08a09517d5f9f1c233a6ff1c7761883f9cd
Jonathan <djanatyn@gmail.com>
2011-08-19 23:36:50 -0400

    maps, maps, maps!

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame
-
+import maps
 pygame.init()
 
 size = 640, 480
@@ -10,19 +10,33 @@ floor = pygame.image.load("floor.png")
 
 screen = pygame.display.set_mode(size)
 
-testmap = [
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+pwalkfront = pygame.image.load("player_front.png")
+pwalkright = pygame.image.load("player_right.png")
+pwalkback = pygame.image.load("player_back.png")
+pwalkleft = pygame.image.load("player_left.png")
+
+class Player:
+    def __init__(self,posx,posy):
+        self.posx = posx
+        self.posy = posy
+        self.position = [posx * 40, posy * 40]
+        self.image = pwalkfront
+        self.location = maps.testmap.array
+    def update(self):
+        self.position = [self.posx * 40, self.posy * 40]
+        screen.blit(self.image,self.position)
+    def run(self,event):
+        potx = 0; poty = 0
+        if event.key == pygame.K_LEFT:    potx = -1; self.image = pwalkleft
+        elif event.key == pygame.K_RIGHT: potx = 1;  self.image = pwalkright
+        elif event.key == pygame.K_UP:    poty = -1; self.image = pwalkback
+        elif event.key == pygame.K_DOWN:  poty = 1;  self.image = pwalkfront
+        
+        if self.location[self.posy + poty][self.posx + potx] != 1:
+            self.posx += potx; self.posy += poty
+    
+frogman = Player(5,5)
+gamequit = 0
 
 def showmap(mapname):
     row = 0; column = 0;
@@ -30,18 +44,24 @@ def showmap(mapname):
         column = 0
         for tile in maprow:
             if tile == 1:
-                print "wall at (%s,%s)" % (row,column)
+              # print "wall at (%s,%s)" % (row,column)
                 screen.blit(wall,[column * 40, row * 40])
             elif tile == 0:
-                print "floor at (%s,%s)" %  (row,column)
+              # print "floor at (%s,%s)" %  (row,column)
                 screen.blit(floor,[column * 40, row * 40])
             column += 1
         row += 1
 
-while True:
+def getinput():
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            frogman.run(event)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE: pygame.quit(); gamequit = 1
+
+while not gamequit:
     clock.tick(30)
-    showmap(testmap)
+    getinput()
+    showmap(frogman.location)
+    frogman.update()
     pygame.display.flip()
-
-
-
