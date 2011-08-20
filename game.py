6aad8deefaca7b767cdf61dfb2e406849eb9bcfa
Jonathan <djanatyn@gmail.com>
2011-08-20 03:27:50 -0400

    going to sleep

--- game.py
+++ game.py
@@ -29,14 +29,17 @@ class Player:
         self.posy = posy
         self.position = [posx * 40, posy * 40]
         self.image = pwalkfront
-        self.location = maps.testmap.array
+        self.location = maps.testmap
         self.coin = 0
     def update(self):
         self.position = [self.posx * 40, self.posy * 40]
-        if self.location[self.posy][self.posx] == 2:
+        if self.location.array[self.posy][self.posx] == 2:
             ping.play()
-            self.location[self.posy][self.posx] = 0
+            self.location.array[self.posy][self.posx] = 0
             self.coin += 1
+        if self.location.array[self.posy][self.posx] > 2:
+            newmap = self.location.array[self.posy][self.posx]
+            
         screen.blit(self.image,self.position)
     def run(self,event):
         potx = 0; poty = 0
@@ -45,11 +48,11 @@ class Player:
         elif event.key == pygame.K_UP:    poty = -1; self.image = pwalkback
         elif event.key == pygame.K_DOWN:  poty = 1;  self.image = pwalkfront
 
-        if self.location[self.posy + poty][self.posx + potx] != 1:
+        if self.location.array[self.posy + poty][self.posx + potx] != 1:
             self.posx += potx; self.posy += poty
         else:
             bump.play()
-    
+
 frogman = Player(5,5)
 gamequit = 0
 
@@ -59,13 +62,11 @@ def showmap(mapname):
         column = 0
         for tile in maprow:
             if tile == 1:
-              # print "wall at (%s,%s)" % (row,column)
                 screen.blit(wall,[column * 40, row * 40])
-            elif tile == 0:
-              # print "floor at (%s,%s)" %  (row,column)
-                screen.blit(floor,[column * 40, row * 40])
             elif tile == 2:
                 screen.blit(coin,[column * 40, row * 40])
+            else:
+                screen.blit(floor,[column * 40, row * 40])
             column += 1
         row += 1
 
@@ -85,7 +86,7 @@ pygame.mixer.music.play(-1)
 while not gamequit:
     clock.tick(30)
     getinput()
-    showmap(frogman.location)
+    showmap(frogman.location.array)
     frogman.update()
     scoreboard()
     pygame.display.flip()
