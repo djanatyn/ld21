f94196d5d32d3092588de043b06994db8a16eaff
Jonathan <djanatyn@gmail.com>
2011-08-21 20:01:13 -0400

    finished

--- game.py
+++ game.py
@@ -11,6 +11,8 @@ pygame.key.set_repeat(100,100)
 wall = pygame.image.load("wall.png")
 floor = pygame.image.load("floor.png")
 coin = pygame.image.load("coin.png")
+floor = pygame.image.load("floor.png")
+door = pygame.image.load("door.png")
 title = pygame.image.load("title.png")
 gui = pygame.image.load("gui.png")
 
@@ -18,6 +20,10 @@ screen = pygame.display.set_mode(size)
 
 bump = pygame.mixer.Sound("bump.wav")
 ping = pygame.mixer.Sound("coin.wav")
+alert = pygame.mixer.Sound("alert.wav")
+needmore = pygame.mixer.Sound("needmore.wav")
+unlock = pygame.mixer.Sound("unlock.wav")
+caught = pygame.mixer.Sound("caught.wav")
 
 score = pygame.font.Font("visitor1.ttf",18)
 
@@ -27,6 +33,7 @@ pwalkback = pygame.image.load("player_back.png")
 pwalkleft = pygame.image.load("player_left.png")
 
 enemy = pygame.image.load("robot.png")
+sleep = pygame.image.load("robot_sleep.png")
 
 class Player:
     def __init__(self,posx,posy):
@@ -42,11 +49,21 @@ class Player:
             ping.play()
             self.location.array[self.posy][self.posx] = 0
             self.coin += 1
-        if self.location.array[self.posy][self.posx] > 2:
+        if self.location.array[self.posy][self.posx] > 3:
             newmap = self.location.array[self.posy][self.posx]
             self.posx, self.posy = self.location.exits[newmap][1]
             mapnum = self.location.exits[newmap][0]
             self.location = maps.list[mapnum]
+            robot.posx = self.location.robot[0]
+            robot.posy = self.location.robot[1]
+            robot.sleep = True
+        if self.position == robot.position:
+            caught.play()
+            self.location = maps.list[0]
+            self.posx = 5; self.posy = 5
+            robot.sleep = True
+            robot.posx = self.location.robot[0]
+            robot.posy = self.location.robot[1]
         screen.blit(self.image,self.position)
     def run(self,event):
         potx = 0; poty = 0
@@ -64,7 +81,18 @@ class Player:
             self.image = pwalkfront
         if potx != 0 or poty != 0:
             if self.location.array[self.posy + poty][self.posx + potx] != 1:
+                if self.location.array[self.posy + poty][self.posx + potx] == 3:
+                    if self.coin > 99:
+                        self.coin += -100
+                        self.location.array[self.posy + poty][self.posx + potx] = 0
+                        unlock.play()
+                    else:
+                        potx = 0; poty = 0
+                        needmore.play()
                 self.posx += potx; self.posy += poty
+                if robot.sleep == True:
+                    robot.sleep = False
+                    alert.play()
             else:
                 bump.play()
 class Enemy:
@@ -73,9 +101,13 @@ class Enemy:
         self.posy = posy
         self.position = [posx * 30, posy * 30]
         self.time = 0
+        self.sleep = True
     def move(self):
-        if self.time > 250:
+        if self.sleep == True:
+            self.time = 0
+        if self.time > 350:
             self.time = 0; potx = 0; poty = 0
+
             if frogman.posx > self.posx: potx = 1
             if frogman.posx < self.posx: potx = -1
             if frogman.posy > self.posy: poty = 1
@@ -86,10 +118,11 @@ class Enemy:
                 self.posx += potx
     def update(self):
         self.position = [self.posx * 30, self.posy * 30]
-        screen.blit(enemy,self.position)
+        if self.sleep == True: screen.blit(sleep,self.position)
+        if self.sleep == False: screen.blit(enemy,self.position)
 
 frogman = Player(5,5)
-badguy = Enemy(10,10)
+robot = Enemy(10,10)
 gamequit = 0
 
 def showmap(mapname):
@@ -101,6 +134,8 @@ def showmap(mapname):
                 screen.blit(wall,[column * 30, row * 30])
             elif tile == 2:
                 screen.blit(coin,[column * 30, row * 30])
+            elif tile == 3:
+                screen.blit(door,[column * 30, row * 30])
             else:
                 screen.blit(floor,[column * 30, row * 30])
             column += 1
@@ -117,15 +152,15 @@ def scoreboard():
     text = score.render("coins: %s" % (frogman.coin),0,(0,0,0))
     screen.blit(text,(500,10))
 
-pygame.mixer.music.load("title.wav")
+pygame.mixer.music.load("room1.wav")
 pygame.mixer.music.play(-1)
 while not gamequit:
-    badguy.time += clock.tick(30)
+    robot.time += clock.tick(30)
     getinput()
     showmap(frogman.location.array)
     frogman.update()
-    badguy.move()
-    badguy.update()
+    robot.move()
+    robot.update()
     screen.blit(title,(0,360))
     screen.blit(gui,(480,0))
     scoreboard()
